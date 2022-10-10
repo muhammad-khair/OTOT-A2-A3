98980996a2a2e44e35504465615da160c80c35cd
muhammad-khair <muhammad_khair@outlook.com>
2022-10-11 00:36:44 +0800

    Add local pull logic

--- demo/a3_demo.sh
+++ demo/a3_demo.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env bash
 
 run() {
-    kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
-    kubectl -nkube-system edit deploy/metrics-server
-    ## maunally add a flag --kubelet-insecure-tls to deployment.spec.containers[].args[] (around line 45)
-    kubectl -nkube-system rollout restart deploy/metrics-server
+    kind create cluster --name kind-1 --config kind/cluster-config.yaml
+    ## review
+    docker ps
+    kubectl get nodes
+    kubectl cluster-info
+
+    kubectl apply -f manifests/k8s/backend-zone-aware.yaml
+    sleep 3
+    kubectl wait --for=condition=ready pod -l app=backend-zone-aware --timeout=180s
+    ## review
+    kubectl get deployment/backend-zone-aware
+
+    kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+    sleep 3
+    kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=180s
+    ## review
+    kubectl -n ingress-nginx get deploy
+
+    kubectl apply -f manifests/k8s/service-zone-aware.yaml
+    ## review
+    kubectl get svc 
+
+    kubectl apply -f manifests/k8s/ingress-zone-aware.yaml
+    ## review
+    kubectl get ingress 
+    kubectl get nodes
 
     kubectl apply -f manifests/k8s/backend-hpa.yaml
     sleep 3
@@ -14,19 +36,27 @@ run() {
     kubectl get po
     kubectl describe hpa
     kubectl get nodes -L topology.kubernetes.io/zone
+}
 
-    kubectl apply -f manifests/k8s/backend-zone-aware.yaml
-    sleep 3
-    kubectl wait --for=condition=ready pod -l app=backend-zone-aware --timeout=180s
-    ## review
-    kubectl get deployment/backend-zone-aware
-    kubectl get po -l app=backend-zone-aware -owide --sort-by='.spec.nodeName'
+delete() {
+    kind delete cluster --name kind-1
 }
 
 main() {
-    set -x
-    run
-    exit
+    if [ $# -eq 0 ]; then
+        set -x
+        run
+        exit
+    elif [ $1 == "d" ]; then
+        set -x
+        delete
+        exit 
+    elif [ $1 == "r" ]; then
+        set -x
+        delete
+        run
+        exit
+    fi
 }
 
-main
+main $@
